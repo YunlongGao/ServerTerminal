727da55748f706d17dd7cb8e0b58a92d2bc20770
高Alonzo <424110808@qq.com>
2018-08-21 15:06:01 +0100

    s-encryption-first commit

---
+++ src/socketserver/socketThread.java
@@ -0,0 +1,19 @@
+package socketserver;
+
+
+public class socketThread implements Runnable{
+	String Myname = null;
+	
+	public socketThread(String Myname) {
+		this.Myname = Myname;
+	}
+	public void clientThread(String Myname){
+		SocketServer ss = new SocketServer(Myname);
+	}
+	
+	@Override
+	public void run() {
+		clientThread(Myname);		
+	}
+
+}
